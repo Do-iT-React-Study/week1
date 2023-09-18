d98e1a9bce05d9f5abb8b71613c7ba3eb3766340
chamny20 <chamny20@naver.com>
2023-09-19 00:43:17 +0900

    [chaemin] : JSX 관련 스터디 자료 업로드

--- src/JSX.js
+++ src/JSX.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function JSX() {
-  return <div>JSX란?</div>;
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <h2>잘 작동하나요?</h2>
+      <h3>만나서 반갑습니당 :></h3>
+    </div>
+  );
 }
