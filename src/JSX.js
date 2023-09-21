4a4009ec801957ec6f930a25c08374329d119f92
chamny20 <chamny20@naver.com>
2023-09-21 18:57:42 +0900

    1회차 스터디 완료

--- src/JSX.js
+++ src/JSX.js
@@ -1,11 +1,27 @@
 import React from "react";
+import "./JSX.css";
+import styled from "styled-components";
+import Chaemin from "./chaemin";
 
 export default function JSX() {
+  const name = "reactt";
+
   return (
     <div>
-      <h1>Hello React!</h1>
+      {name == "react" && <h1>Hello {name}!</h1>}
       <h2>잘 작동하나요?</h2>
       <h3>만나서 반갑습니당 :></h3>
+      <input />
+      <br />
+      <br />
+      <HelloStyled>Hello World!</HelloStyled>
+      <Chaemin />
     </div>
   );
 }
+
+const HelloStyled = styled.div`
+  background-color: skyblue;
+  font-size: 100px;
+  color: white;
+`;
---
+++ src/chaemin.js
@@ -0,0 +1,3 @@
+export default function Chaemin() {
+  return <div>chaemin</div>;
+}
