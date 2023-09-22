57dc557ba8c401c759ad2b4dd0c99f687ba85c8f
jangyootae <sjang1425@naver.com>
2023-09-22 16:46:40 +0900

    [yootae] : 1주차 출석과제 완료

--- src/JSX.js
+++ src/JSX.js
@@ -1,11 +1,29 @@
 import React from "react";
+import "./JSX.css";
+import styled from "styled-components";
+import Yootae from "./Yootae"
+
 
 export default function JSX() {
+
+  const name = "ReacT";
+
   return (
     <div>
-      <h1>Hello React!</h1>
+      {name == "reacT" ? <h1>is react</h1>:null}
+      <h1>Hello {name}!</h1>
       <h2>잘 작동하나요?</h2>
-      <h3>만나서 반갑습니당 :></h3>
+      <h3>만나서 반갑습니당 :</h3>
+      <input />
+      <br />
+      <br />
+      <Hellostyled>Hello</Hellostyled>
+      <Yootae />
     </div>
   );
 }
+const Hellostyled = styled.div`
+  background-color:skyblue;
+  font-size: 500px;
+  color: white;
+`;
